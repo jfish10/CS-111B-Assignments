e66b280c73087f3ea58a6968bf07341d24cfbfa4
Jonny <everestmf8@gmail.com>
2019-02-04 09:53:06 -0800

    Update PalindromePyramid.java

--- Assignment 2/Palindrome Pyramid Assignment 2/src/PalindromePyramid.java
+++ Assignment 2/Palindrome Pyramid Assignment 2/src/PalindromePyramid.java
@@ -8,11 +8,11 @@ class PalindromePyramid {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a number [1-7] and [8] to quit: ");
+        System.out.println("Enter a number [1-7] and [8] to quit: "); //user can print out a pyramid up to 7 levels only, per assignment specs
         int steps = Integer.parseInt(scan.nextLine());
 
 
-        while (steps >= 1 && steps <= 7) {
+        while (steps >= 1 && steps <= 7) { //int variables used to store values for each level in the pyramid. Long types needed for the larger values in steps 6 and 7. 
             int levelOne = 1;
             int levelTwo = (levelOne*10+1) * (levelOne*10+1);
             int levelThree = (levelOne*100+11) * (levelOne*100+11);
@@ -21,34 +21,34 @@ class PalindromePyramid {
             long levelSix = (levelOne*100000L+11111L) * (levelOne*100000L+11111L);
             long levelSeven = (levelOne*1000000L+111111L) * (levelOne*1000000L+111111L);
 
-            if(steps == 8){
+            if(steps == 8){ // allows user to exit program with printing anything
                 System.out.println("Quitting program. Run again to continue.");
                 break;
             }
 
-            if (steps == 1) {
+            if (steps == 1) { //Level 1 of the palindrome pyramid
                 System.out.println("         " + levelOne);
                 break;
             }
-            else if (steps == 2) {
+            else if (steps == 2) { //Level 2 of the palindrome pyramid
                 System.out.println("         " + levelOne);
                 System.out.println("        " + levelTwo);
                 break;
             }
-            else if (steps == 3) {
+            else if (steps == 3) { //Level 3 of the palindrome pyramid
                 System.out.println("         " + levelOne);
                 System.out.println("        " + levelTwo);
                 System.out.println("       " + levelThree);
                 break;
             }
-            else if (steps ==4) {
+            else if (steps ==4) { // Level 4 of the palindrome pyramid
                 System.out.println("         " + levelOne);
                 System.out.println("        " + levelTwo);
                 System.out.println("       " + levelThree);
                 System.out.println("      " + levelFour);
                 break;
             }
-            else if (steps == 5) {
+            else if (steps == 5) { //Level 5 of the palindrome pyramid
                 System.out.println("         " + levelOne);
                 System.out.println("        " + levelTwo);
                 System.out.println("       " + levelThree);
@@ -56,7 +56,7 @@ class PalindromePyramid {
                 System.out.println("     " + levelFive);
                 break;
             }
-            else if (steps == 6) {
+            else if (steps == 6) { //Level 6 of the palindrome pyramid. Long values utilized here.
                 System.out.println("      " + levelOne);
                 System.out.println("     " + levelTwo);
                 System.out.println("    " + levelThree);
@@ -65,7 +65,7 @@ class PalindromePyramid {
                 System.out.println(" " + levelSix);
                 break;
             }
-            else if (steps == 7){
+            else if (steps == 7){ //Level 7 of the palindrome pyramid. Long values utilized here.
                 System.out.println("         " + levelOne);
                 System.out.println("        " + levelTwo);
                 System.out.println("       " + levelThree);
