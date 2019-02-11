e0e1961400f31239b9932a2d12c15dc36c721a59
Jonny <everestmf8@gmail.com>
2019-02-11 14:53:14 -0800

    Update twentyOneCardMagic.java

--- Assignment 3/Magic Trick/src/twentyOneCardMagic.java
+++ Assignment 3/Magic Trick/src/twentyOneCardMagic.java
@@ -1,26 +1,37 @@
 import java.util.Scanner;
-import java.util.Random;
-
+import java.utils.Arrays;
 
 public class twentyOneCardMagic {
     public static void main(String[] args) {
-        Random rn = new Random();
 
         /* Declare a 21 element array of cards */
-        int[] deck = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21};
+        int[] deck1 = {1,2,3,4,5,6,7};
+        int[] deck2 = {8,9,10,11,12,13,14};
+        int[] deck3 = {15,16,17,18,19,20,21};
+
 
         /* Declare a 7 by 3 array to receive the cards dealt to play the trick */
-        int[][] array = new int[7][3];
+        // int[][] array = new int[7][3];
 
         /* Declare a Scanner object for input */
         Scanner input = new Scanner(System.in);
 
 
+        System.out.println("Before you begin, please think of a number between 1 and 21, inclusive.");
+        System.out.println("Press [0]-[enter] to begin the game/trick.");
 
-        System.out.println("Press [0] to begin the game/trick.");
         int play = input.nextInt();
 
         if (play == 0) {
+
+
+
+          System.out.println(Arrays.toString(deck1));
+          System.out.println(Arrays.toString(deck2));
+          System.out.println(Arrays.toString(deck3));
+
+          System.out.println("Which pile does your card reside in? Enter [1], [2], or [3]:");
+          int pile = input.nextInt();
 
 
             //use 2 for loops to populate the cards
